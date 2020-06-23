350e6d1e4939070aeb69d5f4aee3c3b23a306799
katemolina <katemolina@curo.com>
2020-06-23 18:27:22 -0500

    FileStream ReadAndWrite Added

--- HomeWork5_Threading/Program.cs
+++ HomeWork5_Threading/Program.cs
@@ -9,54 +9,34 @@ namespace HomeWork5_Threading
 {
     class Program
     {
+        public static AutoResetEvent _event = new AutoResetEvent(false);
         static void Main(string[] args)
         {
+            CalculationAndIOTaskClass calcIOClass = new CalculationAndIOTaskClass();
+            
+
+    // 1.
             Console.WriteLine("Enter a number! to calculate a factorial for:");
             object n = Convert.ToInt32(Console.ReadLine());
-            var thread = new Thread(new ParameterizedThreadStart(GetFactorial));
+            var thread = new Thread(new ParameterizedThreadStart(calcIOClass.GetFactorial));
             thread.Start(n);
             Console.WriteLine("Wait for the thread to complete");
             Console.ReadLine();
             _event.Set();
             //  ------------------------------------------------------------
-           
-                Console.WriteLine("Enter a number to calculate the sum of the whole numbers before:");
-                object n1 = Convert.ToInt32(Console.ReadLine());
-                var thread2 = new Thread(new ParameterizedThreadStart(GetWholeNumber));
-                thread2.Start(n1);
-                Console.WriteLine("Wait for the thread to complete");
+    // 2.
+            Console.WriteLine("Enter a number to calculate the sum of the whole numbers before:");
+            object n1 = Convert.ToInt32(Console.ReadLine());
+            var thread2 = new Thread(new ParameterizedThreadStart(calcIOClass.GetWholeNumber));
+            thread2.Start(n1);
+            Console.WriteLine("Wait for the thread to complete");
             Console.ReadLine();
             _event.Set();
+            //  ------------------------------------------------------------
+    // 3. 
+            calcIOClass.ReadFromCSV();
 
         }
-        public static AutoResetEvent _event = new AutoResetEvent(false);
-        private static void GetFactorial(object n)
-        {
-            
-                int n1 = (int)n;
-                long k = n1;
-                for (int i = n1 - 1; i >= 1; i--)
-                {
-                    k = k * i;
-                }
-                Console.WriteLine("The factorial of {0} is {1}", n1, k);
-            
-        }
-        private static object lockObj = new object();
-        private static void GetWholeNumber(object n)
-        {
-            
-                int n1 = (int)n;
-                long k = n1;
-                for (int i = n1 - 1; i > 0; i--)
-                {
-
-                    k = k + i;
-                }
-                Console.WriteLine("The sum of the whole number{0} is {1}", n1, k);
-            
-        }
-
 
     }
 }
