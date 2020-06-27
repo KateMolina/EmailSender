2fac4c276262da0d8dfce8ea48c826122b73f38d
katemolina <katemolina@curo.com>
2020-06-26 21:55:48 -0500

    HW6_ParralelMatrixMultiplication

---
+++ HomeWork6_Parallel/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6_Parallel
+{
+    class Program
+    {
+       
+
+
+        static void Main(string[] args)
+        {
+            int r = 5;
+            int c = 5;
+            int[,] matrix1 = new int[r,c];
+            int[,] matrix2 = new int[r, c];
+            int[,] matrix3 = new int[r, c];
+
+
+            MultiplyingMatrices matr = new MultiplyingMatrices();
+
+          
+            matr.FillingMatrices(matrix1, matrix2);
+           // matr.Display(matrix1);
+            //matr.Display(matrix2);
+            matr.Display(matr.Multiplication(matrix1, matrix2));
+
+           
+
+
+
+
+
+        }
+    }
+}
